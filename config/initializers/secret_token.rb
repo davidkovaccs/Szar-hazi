ca3156a5e833814ddb82987797a4315501d2ab63
Barta Pal <brazil.hu@gmail.com>
2011-11-17 01:31:22 +0100

    Alap authentikacio fileokkal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SzarHazi::Application.config.secret_token = 'acb045fd5c72459f888c8a08b060bb3f8cafcebaeca353c03cccafdfe10a09f6d9f79c863fcac065a76010c6335fc68f8571c31f94a5703b4397afe749e4887b'
