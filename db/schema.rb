70118597970dd3855c94c506bcfd50b796c4fdcc
Barta Pal <brazil.hu@gmail.com>
2011-11-20 00:04:18 +0100

    users list, user last name, first name

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111119101539) do
+ActiveRecord::Schema.define(:version => 20111119204611) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -56,6 +57,8 @@ ActiveRecord::Schema.define(:version => 20111119101539) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
