4393b1a1992c984ee66eb0a17d21f4a257d528f1
David Kovacs <davidkovaccs@gmail.com>
2011-11-20 11:38:08 +0100

    sell/buy buttons are added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,45 @@ class HomeController < ApplicationController
     end
   end
 
-  def show_accounts
+  def buy
+    if current_user.accounts.empty? then
+      flash[:alert] = "Nincs még számlája, hozzon létre egyet, hogy megbízást tudjon kötni."
+      redirect_to :controller => "accounts", :action => 'new'
+      return
+    end
+
+    if current_user.accounts.find_by_active(1)
+      selected = current_user.accounts.find_by_active(1).id
+    else
+      selected = current_user.accounts.first.id
+    end
+
+    Order.create(  :account_id => selected,
+      :stock_id => params[:stock_id],
+      :price => Stock.find(params[:stock_id]).price,
+      :sell => 0)
+
+    redirect_to :controller => 'orders', :action => 'index'
   end
 
-  def show_orders
-  end
+  def sell
+    if current_user.accounts.empty? then
+      flash[:alert] = "Nincs még számlája, hozzon létre egyet, hogy megbízást tudjon kötni."
+      redirect_to :controller => "accounts", :action => 'new'
+      return
+    end
 
-  def new_order
-    @order = Order.new
+    if current_user.accounts.find_by_active(1)
+      selected = current_user.accounts.find_by_active(1).id
+    else
+      selected = current_user.accounts.first.id
+    end
+
+    Order.create(  :account_id => selected,
+      :stock_id => params[:stock_id],
+      :price => Stock.find(params[:stock_id]).price,
+      :sell => 1)
+
+    redirect_to :controller => 'orders', :action => 'index'
   end
 end
